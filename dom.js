deb0b4456ec60669618b7ebc367dd52adf30ccf3
Nathalie Lukubisa <nlukubisa@gmail.com>
2019-01-13 12:20:49 +0200

    first commit of the index.html with bootstrap and dom.js witch is about the DOM

---
+++ dom.js
@@ -0,0 +1,47 @@
+var form=document.querySelector('#addForm');
+var itemList=document.querySelector("#items");
+var filter=document.querySelector('#filter');
+form.addEventListener('submit',addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup',filterItems);
+
+function addItem(e){
+    e.preventDefault();
+    var newItem = document.querySelector("#item").value;
+   var li = document.createElement('li');
+   li.className="list-group-item";
+   li.appendChild(document.createTextNode(newItem));
+   var deleteButton=document.createElement('button');
+  deleteButton.className="btn btn-dark btn-sm float-right delete";
+  deleteButton.appendChild(document.createTextNode("X"));
+  
+  li.appendChild(deleteButton);
+  itemList.appendChild(li);
+}
+
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure you wanna delete this item?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+  }
+}
+
+function filterItems(e){
+    var text= e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+        var itemName= item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) !=-1){
+            item.style.display="block";
+
+        }else{
+            item.style.display="none";
+
+        }
+
+    });
+
+}
